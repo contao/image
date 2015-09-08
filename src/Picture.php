a2cf21f983d2f697823527aff1c622305ea01544
Martin Auswöger <martin@auswoeger.com>
2015-09-08 21:58:47 +0200

    Add image services

---
+++ src/Picture.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\CoreBundle\Image;
+
+/**
+ * Resizes images and creates picture data
+ *
+ * @author Martin Auswöger <martin@auswoeger.com>
+ */
+class Picture
+{
+}
