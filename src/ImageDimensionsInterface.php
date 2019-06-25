092c5ba58219714c928a854567bd6c2fe5d64827
Martin Auswöger <martin@auswoeger.com>
2019-06-25 12:10:55 +0200

    Comment where orientation constants come from

--- src/ImageDimensionsInterface.php
+++ src/ImageDimensionsInterface.php
@@ -16,6 +16,11 @@ use Imagine\Image\BoxInterface;
 
 interface ImageDimensionsInterface
 {
+    /**
+     * Exif 2.32 orientation attribute (tag 274).
+     *
+     * @see <http://www.cipa.jp/std/documents/e/DC-008-Translation-2019-E.pdf>
+     */
     public const ORIENTATION_NORMAL = 1;
     public const ORIENTATION_NORMAL_90 = 6;
     public const ORIENTATION_NORMAL_180 = 3;
